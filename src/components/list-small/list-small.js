25a8f517e6b5f985e3b1d5304b1a4154bf03d0bb
xfront <xfront@126.com>
2017-10-14 18:46:40 +0800

    2

--- src/components/list-small/list-small.js
+++ src/components/list-small/list-small.js
@@ -7,21 +7,21 @@ export default React.createClass({
         let item = this.props.scontent;
         let itemid = '1';
         return (
-                    <li className="list-small">
-                        <div className="left">
-                            <div className="title">
-                                <Link to={{ path:'/detail'}} >{item.title}</Link>
-                            </div>
-                            <div className="info">
-                                <span className="source">{item.source}</span>
-                                <div className="comment"><span>{item.commentnum}</span><span>评论</span></div>
-                                <span className="forbid">x</span>
-                            </div>
+                <li className="list-small">
+                    <div className="left">
+                        <div className="title">
+                            <Link to={{ path:'/detail'}} >{item.title}</Link>
                         </div>
-                        <div className="right">
-                            <img className="pic" src={item.imgsrc}/>
+                        <div className="info">
+                            <span className="source">{item.source}</span>
+                            <div className="comment"><span>{item.commentnum}</span><span>评论</span></div>
+                            <span className="forbid">x</span>
                         </div>
-                    </li>
+                    </div>
+                    <div className="right">
+                        <img className="pic" src={item.imgsrc}/>
+                    </div>
+                </li>
             
         );
     }
